769bc783141f4920806b3699ff19c709bdb590a8
Blue1818 <70177582+Blue1818@users.noreply.github.com>
2020-10-28 14:44:35 -0500

    function foundation
    layed foundation for functions

--- z1910234db.sh
+++ z1910234db.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
+#define fuctions:
+
+#This function is used to create a new database file. It takes up to two parameters:
+#$1 The filename to use for the new database file. This must be specified
+#$2 The label to put on the first line of the new database file.
+#   If not specified, use "Untitled database" instead.
+new () 
+{
+
+}
+
+add ()
+{
+
+}
+
+show ()
+{
+
+}
+
+delete()
+{
+
+}
+
+count()
+{
+
+
+}
+
 #check if theres any arguments from the command line
 
 #If no arguments are passed:
+
     #Then we are in interactive mode:
     #Prompt the user for commands and assign them to the input string array
 
