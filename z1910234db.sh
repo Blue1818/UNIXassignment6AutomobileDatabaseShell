70c1ee58e47aa69cc4ae927f9e490a5da9420edd
Blue1818 <70177582+Blue1818@users.noreply.github.com>
2020-10-28 14:29:05 -0500

    Update z1910234db.sh

--- z1910234db.sh
+++ z1910234db.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-echo "$1 $2"
+
+#check if theres any arguments from the command line
+
+#If no arguments are passed:
+    #Then we are in interactive mode:
+    #Prompt the user for commands and assign them to the input string array
+
+    #LOOP til input is quit
+        #Execute commands
+
+
+
+        #Prompt the user for commands and assign them to the input string array
+
+    #END LOOP
+
+#Else
+    #Load command line arguments 
+
+
+    #Execute commands
+
+#End If
+
+#exit program
